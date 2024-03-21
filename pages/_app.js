1fff8abc563024d3bbb88e28ecad52f1b8d250c2
madoka <mnogaki@my.bcit.ca>
2024-03-21 16:39:11 -0700

    add DM_Sans font and delete Inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { DM_Sans } from "next/font/google";
+
+const dm_sans = DM_Sans({ subsets: ["latin"]});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={dm_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
