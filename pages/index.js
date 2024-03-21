905456524ed2b9969945dc044db8802d49af1391
madoka <mnogaki@my.bcit.ca>
2024-03-21 16:20:31 -0700

    add global font size

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-       
+       <h1>hello</h1>
       </main>
     </>
   );
