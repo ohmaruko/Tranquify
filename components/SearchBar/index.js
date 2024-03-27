f77469a68d4ae7c77b9beba112e130b8c64afe44
madoka <mnogaki@my.bcit.ca>
2024-03-27 01:28:23 -0700

    build search bar

---
+++ components/SearchBar/index.js
@@ -0,0 +1,33 @@
+import styles from "./SarchBar.module.css";
+import { meditationData } from "@/data/meditation";
+import { useState } from "react";
+import FavouriteCardMedium from "../FavouritesCardMedium";
+
+export default function SearchBar() {
+  const [text, setText] = useState(null);
+
+  return (
+    <>
+      <div>
+        <div className={styles.inputBoxContainer}>
+            <input type="search" 
+                placeholder="Search" 
+                className={styles.inputBox}
+                onChange={ e => setText(e.target.value)}
+                />
+        </div>
+        <div className={styles.searchResult}>
+        {
+            meditationData.meditations.map( x => {
+            return(
+                x.title.includes(text)? 
+                <div className={styles.searchResultSingle}><FavouriteCardMedium title={x.title} time='${x.duration}min' source="#" thumbnail="/images/placeholder.png"/></div>
+                :<></>
+            )
+            })
+        }
+        </div>
+      </div>
+    </>
+  );
+}
