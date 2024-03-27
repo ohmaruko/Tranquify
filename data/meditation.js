f4fab64b937d570e76002bc9ec60571e5a5ec1b0
madoka <mnogaki@my.bcit.ca>
2024-03-27 01:26:20 -0700

    add meditation content data

---
+++ data/meditation.js
@@ -0,0 +1,31 @@
+export const meditationData = {
+    meditations: [
+        {
+            title: "Anxiety Away",
+            duration: 3,
+            isGuided: true,
+            category: "stress",
+            isSaved: true,
+            isOnlyMusic: false,
+            source: "https://youtu.be/EfubAj5f_rM?si=m_RyVXq3-N86vyd0"
+        },
+        {
+            title: "Get Peace",
+            duration: 10,
+            isGuided: true,
+            category: "anger",
+            isSaved: false,
+            isOnlyMusic: false,
+            source: "https://youtu.be/cZJAsW_5SRA?si=kY2zWkQ0jw0-1IPE"
+        },
+        {
+            title: "Forest",
+            duration: 10,
+            isGuided: false,
+            category: "",
+            isSaved: false,
+            isOnlyMusic: true,
+            source: "https://youtu.be/4S3yJkGWM4E?si=3W40JNraFmiirHLP"
+        }
+    ]
+}
