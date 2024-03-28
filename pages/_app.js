b7b549e4a57aaaceef6966b7ff22b5da236ceac7
Yae-Won Lee <ylee384@my.bcit.ca>
2024-03-28 00:22:54 -0700

    Revert "add Quicksand font to app and logo"
    This reverts commit 41113a07ce99af8900dba743d8752b2555b974b6.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
-import { DM_Sans, Quicksand } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
-const dm_sans = DM_Sans({ subsets: ["latin"] });
-const quicksand = Quicksand({ subsets: ["latin"] });
+const dm_sans = DM_Sans({ subsets: ["latin"]});
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${dm_sans.className} ${quicksand.className}`}>
+    <main className={dm_sans.className}>
       <Component {...pageProps} />
     </main>
-  );
+  )
 }
