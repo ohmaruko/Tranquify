41113a07ce99af8900dba743d8752b2555b974b6
Yae-Won Lee <ylee384@my.bcit.ca>
2024-03-28 00:08:16 -0700

    add Quicksand font to app and logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Quicksand } from "next/font/google";
 
-const dm_sans = DM_Sans({ subsets: ["latin"]});
+const dm_sans = DM_Sans({ subsets: ["latin"] });
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={dm_sans.className}>
+    <main className={`${dm_sans.className} ${quicksand.className}`}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
