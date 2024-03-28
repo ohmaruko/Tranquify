0c53f401b410df868d08ce5376a030b3399bd6d7
Yae-Won Lee <ylee384@my.bcit.ca>
2024-03-28 13:16:24 -0700

    add 'X' svg

--- components/SettingPopup/index.js
+++ components/SettingPopup/index.js
@@ -15,7 +15,11 @@ const SettingPopup = () => {
           <div className={styles.backdrop}></div>
           <div className={styles.popup}>
             <button className={styles.closeButton} onClick={handleClosePopup}>
-              X
+                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.80695 4.13232L21.5345 21.9999" stroke="#9DB580" stroke-width="7" stroke-linecap="round"/>
+<path d="M21.55 4L3.99999 21.55" stroke="#9DB580" stroke-width="7" stroke-linecap="round"/>
+                </svg>
+
             </button>
             <svg className={styles.rectangleSvg} fill="none" viewBox="0 0 322 672" xmlns="http://www.w3.org/2000/svg">
               <rect height="672" width="322" fill="white" rx="30"/>
