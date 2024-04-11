5b716e5ebdb28db92950ae2f7d17ccf5665422df
madoka <mnogaki@my.bcit.ca>
2024-04-11 00:09:45 -0700

    stylize search bar and its overlay

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,33 +1,56 @@
 import styles from "./SarchBar.module.css";
 import { meditationData } from "@/data/meditation";
-import { useState } from "react";
-import FavouriteCardMedium from "../MeditationCardMedium";
+import { useState, useEffect } from "react";
+import Image from 'next/image'
+import MeditationCardMedium from "../MeditationCardMedium";
 
 export default function SearchBar() {
-  const [text, setText] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(isOpen);
+    if(searchInput == "") {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);           
+    }
+  }, [searchInput]);
 
   return (
-    <>
-      <div>
-        <div className={styles.inputBoxContainer}>
-            <input type="search" 
-                placeholder="Search" 
-                className={styles.inputBox}
-                onChange={ e => setText(e.target.value)}
-                />
-        </div>
+    <div className={styles.searchContainer}>
+      <input type="search" 
+          placeholder="Search" 
+          className={styles.inputBox}
+          onChange={ e => {
+            setSearchInput(e.target.value);
+          }}
+          />
+    {
+      isOpen?
         <div className={styles.searchResult}>
-        {
-            meditationData.meditations.map( x => {
-            return(
-                x.title.includes(text)? 
-                <div className={styles.searchResultSingle}><FavouriteCardMedium title={x.title} time='${x.duration}min' source="#" thumbnail="/images/placeholder.png"/></div>
-                :<></>
-            )
-            })
-        }
+          <div className={styles.heading}>
+            <h2>Search Results</h2>
+            <Image src='/images/closeButton.svg' alt="Close Button" width={32} height={32} 
+              onClick={() => setIsOpen(false)}
+            />
+          </div>
+          <div className={styles.meditaions}>
+            {
+              meditationData.meditations.map( x => {
+              return(
+                searchInput.length > 0 && x.title.includes(searchInput)? 
+                    <div className={styles.searchResultSingle}>
+                      <MeditationCardMedium title={x.title} time={x.duration} source={x.source} thumbnail={x.thumbnail}/>
+                    </div>
+                  :<></>
+              )
+              })
+            }
+          </div>
         </div>
-      </div>
-    </>
+        :<></>
+    }
+    </div>
   );
 }
