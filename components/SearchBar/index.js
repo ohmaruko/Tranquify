e98a01cd719b3b4d96a5a11713357382b7e90fe4
madoka <mnogaki@my.bcit.ca>
2024-04-11 10:45:01 -0700

    fix the search result overlay

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -26,31 +26,31 @@ export default function SearchBar() {
             setSearchInput(e.target.value);
           }}
           />
-    {
-      isOpen?
-        <div className={styles.searchResult}>
-          <div className={styles.heading}>
-            <h2>Search Results</h2>
-            <Image src='/images/closeButton.svg' alt="Close Button" width={32} height={32} 
-              onClick={() => setIsOpen(false)}
-            />
+      {
+        isOpen?
+          <div className={styles.searchResult}>
+            <div className={styles.heading}>
+              <h2>Search Results</h2>
+              <Image src='/images/closeButton.svg' alt="Close Button" width={32} height={32} 
+                onClick={() => setIsOpen(false)}
+              />
+            </div>
+            <div className={styles.meditaions}>
+              {
+                meditationData.meditations.map( x => {
+                return(
+                  searchInput.length > 0 && x.title.toLocaleLowerCase().includes(searchInput)? 
+                      <div className={styles.searchResultSingle}>
+                        <MeditationCardMedium title={x.title} time={x.duration} source={x.source} thumbnail={x.thumbnail}/>
+                      </div>
+                    :<></>
+                )
+                })
+              }
+            </div>
           </div>
-          <div className={styles.meditaions}>
-            {
-              meditationData.meditations.map( x => {
-              return(
-                searchInput.length > 0 && x.title.includes(searchInput)? 
-                    <div className={styles.searchResultSingle}>
-                      <MeditationCardMedium title={x.title} time={x.duration} source={x.source} thumbnail={x.thumbnail}/>
-                    </div>
-                  :<></>
-              )
-              })
-            }
-          </div>
-        </div>
-        :<></>
-    }
+          :<></>
+      }
     </div>
   );
 }
