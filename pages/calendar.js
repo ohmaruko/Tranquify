a4d8312b88f7000390038595280cb1cfed112a8d
Yae-Won Lee <ylee384@my.bcit.ca>
2024-04-24 19:02:19 -0700

    create Calendar component (using react-calendar)

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,11 +1,24 @@
 import Navigation from '@/components/Navigation';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import styles from '@/styles/Calendar.module.css'
+import TopBar from '@/components/TopBar';
 
-export default function Calendar() {
+export default function renderCalendar() {
+  const [date, setDate] = useState(new Date());
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className={styles.container}>
+      <TopBar backButton={true}/>
 
+      {/* <h1>Calendar</h1> */}
+      <div className={styles.calendarContainer}>
+        <Calendar onChange={setDate} value={date} />
+      </div>
 
+      <p className={styles.textCenter}>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
 
 
       <Navigation />
