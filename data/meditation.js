ff81e0cce51726dcd72884fb309dba1bb72e3f74
madoka <mnogaki@my.bcit.ca>
2024-05-03 18:15:10 -0700

    add meditation contents in data file

--- data/meditation.js
+++ data/meditation.js
@@ -7,8 +7,18 @@ export const meditationData = {
             misicType: [],
             isSaved: true,
             source: "EfubAj5f_rM",
-            thumbnail: "/images/thumbnails/anxietyAway.png",
+            thumbnail: "/images/thumbnails/anxiety_away.png",
             description: "This meditation helps ease anxiety and fosters inner peace through gentle guidance, deep breathing, and visualization. It encourages mindfulness and self-care, empowering individuals to navigate life with greater ease."
+        },
+        {
+            title: "Be Present",
+            duration: 10,
+            moodType: ["stress"],
+            misicType: [],
+            isSaved: true,
+            source: "ZToicYcHIOU",
+            thumbnail: "/images/thumbnails/be_present.png",
+            description: "Discover peace in the present moment with this guided meditation. Through gentle guidance, find stillness in mind and body, fostering a deep connection to now. Relax and experience calm and clarity."
         },
         {
             title: "Get Peace",
@@ -17,9 +27,50 @@ export const meditationData = {
             misicType: [],
             isSaved: false,
             source: "cZJAsW_5SRA",
-            thumbnail: "/images/thumbnails/getPeace.png",
+            thumbnail: "/images/thumbnails/get_peace.png",
             description: "This meditation helps ease anxiety and fosters inner peace through gentle guidance, deep breathing, and visualization. It encourages mindfulness and self-care, empowering individuals to navigate life with greater ease."
         },
+        {
+            title: "Breathing Meditation",
+            duration: 10,
+            moodType: ["stress"],
+            misicType: [],
+            isSaved: false,
+            source: "QHXja7FUPJc",
+            thumbnail: "/images/thumbnails/breathing_meditation.png",
+            description: "Relax and center your mind with a soothing guided meditation focusing on the rhythm of your breath. Find tranquility and clarity through simple, mindful breathing techniques."
+        },
+        {
+            title: "Calm Within Chaos",
+            duration: 5,
+            moodType: ["stress"],
+            misicType: [],
+            isSaved: false,
+            source: "inpok4MKVLM",
+            thumbnail: "/images/thumbnails/calm_within_chaos.png",
+            description: "Discover tranquility amidst life's turbulence. Let soothing melodies guide you to a peaceful oasis within. Find solace in the gentle embrace of mindfulness and reclaim balance in \"Calm Within Chaos\"."
+        },
+        {
+            title: "Nighttime Serenity",
+            duration: 15,
+            moodType: ["sleep"],
+            misicType: [],
+            isSaved: false,
+            source: "W19PdslW7iw",
+            thumbnail: "/images/thumbnails/nighttime_serenity.png",
+            description: "Discover tranquility under the stars, guided by gentle whispers of the night. Find solace in quiet reflection, embracing peace and harmony within the darkness's gentle embrace."
+        },
+        {
+            title: "Sleep Oasis",
+            duration: 15,
+            moodType: ["sleep"],
+            misicType: [],
+            isSaved: false,
+            source: "yU4LxOcY5w8",
+            thumbnail: "/images/thumbnails/sleep_oasis.png",
+            description: "Find solace in a peaceful haven of restful calmness. Let go of the day's burdens and find rejuvenation in the tranquility of your own Sleep Oasis."
+        },
+        // Music
         {
             title: "Forest",
             duration: 10,
@@ -28,7 +79,47 @@ export const meditationData = {
             isSaved: false,
             source: "4S3yJkGWM4E",
             thumbnail: "/images/thumbnails/forest.png",
-            description: "This meditation helps ease anxiety and fosters inner peace through gentle guidance, deep breathing, and visualization. It encourages mindfulness and self-care, empowering individuals to navigate life with greater ease."
-        }
+            description: "Find solace in rustling leaves, babbling brooks, and birdsong, a serene escape into nature's embrace."
+        },
+        {
+            title: "River",
+            duration: 20,
+            moodType: ["sleep"],
+            misicType: ["nature"],
+            isSaved: false,
+            source: "KqivhqCjPNA",
+            thumbnail: "/images/thumbnails/river.png",
+            description: "Find peace in the gentle rush of water, bird songs, and rustling leaves of this soothing meditation."
+        },
+        {
+            title: "Waves",
+            duration: 10,
+            moodType: ["sleep"],
+            misicType: ["nature"],
+            isSaved: false,
+            source: "mxXV7pKD3nk",
+            thumbnail: "/images/thumbnails/waves.png",
+            description: "Connect with nature's rhythm through gentle ocean waves, a soothing melody for mindfulness and relaxation."
+        },
+        {
+            title: "Natural Harmony",
+            duration: 20,
+            moodType: ["sleep"],
+            misicType: ["nature"],
+            isSaved: false,
+            source: "hxIlvfTDNuE",
+            thumbnail: "/images/thumbnails/natural_harmony.png",
+            description: "Find peace in the gentle rustle of leaves, flowing water, and birdsong, embracing nature's symphony."
+        },
+        {
+            title: "Birds",
+            duration: 20,
+            moodType: ["sleep"],
+            misicType: ["nature"],
+            isSaved: false,
+            source: "43AtY5W3rWA",
+            thumbnail: "/images/thumbnails/birds.png",
+            description: "Discover a tapestry of chirping melodies and rustling leaves, a symphony of nature's tranquility."
+        },
     ]
 }
