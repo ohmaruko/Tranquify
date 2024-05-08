15893fe00a8904c63bc7fbd620ee3a3e72bc997d
Yae-Won Lee <ylee384@my.bcit.ca>
2024-05-07 23:31:01 -0700

    create SettingButton02 component & update CSS styles

--- components/SettingButton/index.js
+++ components/SettingButton/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styles from "./SettingButton.module.css";
-import ToggleButton from "../ToggleButton";
 
 const SettingButton = ({ title, icon, toggle, chevron, children }) => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
---
+++ components/SettingButton02/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styles from "./SettingButton02.module.css";
+
+const SettingButton02 = ({ title, icon, link, toggle }) => {
+
+
+    return (
+        <div>
+            <a href={link} className={styles.button}>
+                <div className={styles.iconAndTitle}>
+                     <div className={styles.icon}>
+                        {icon}
+                    </div>
+                    <div className={styles.title}>
+                        {title}
+                    </div>
+                </div>  
+
+                <div className={styles.toggle}>
+                     {toggle}
+                </div>
+            </a>
+        </div>
+    );
+};
+
+export default SettingButton02;
